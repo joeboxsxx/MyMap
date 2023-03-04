ce0d4f7fecc44adfab905e248353b38e469a2160
joeboxsxx <joeboxsxx@ezweb.ne.jp>
2023-03-05 00:36:57 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by 戸上健太 on 2023/03/05.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
